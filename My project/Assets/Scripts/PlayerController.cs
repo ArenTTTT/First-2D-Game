34d8a7e8a4417253f79499c36bb049a1da959f65
ArenTTTT <111304413+ArenTTTT@users.noreply.github.com>
2022-09-15 09:36:03 -0700

    level creation

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -52,6 +52,12 @@ public class PlayerController : MonoBehaviour
             theRB2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, theRB2D.velocity.y);
 
             theAnimator.SetFloat("Speed", Mathf.Abs(theRB2D.velocity.x));
+
+            if(theRB2D.velocity.x > 0)
+                transform.localScale = new Vector2(1, 1f);
+            else if(theRB2D.velocity.x < 0)
+                transform.localScale = new Vector2(-1f, 1f);
+                
         }
     }
 
