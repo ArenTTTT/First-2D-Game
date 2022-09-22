9e2a6389aba7c3130d557b4ddfe968aa4f8f7335
ArenTTTT <111304413+ArenTTTT@users.noreply.github.com>
2022-09-22 12:04:57 -0700

    fixed?

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -53,11 +53,14 @@ public class PlayerController : MonoBehaviour
             theRB2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, theRB2D.velocity.y);
 
             theAnimator.SetFloat("Speed", Mathf.Abs(theRB2D.velocity.x));
+<<<<<<< HEAD
 
             if (theRB2D.velocity.x > 0)
                 transform.localScale = new Vector2(1f, 1f);
             else if (theRB2D.velocity.x < 0)
                 transform.localScale = new Vector2(-1f, 1f);    
+=======
+>>>>>>> parent of 34d8a7e (level creation)
         }
     }
 
